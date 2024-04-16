ea7dd15941e060ddfb831f6598940e267558f80c
Luberski <Luberski@proton.me>
2024-04-16 16:35:20 +0200

    implemented put option

--- prog.c
+++ prog.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <threads.h>
 
+const char *KV_DATABASE_PATH = "kv_database.txt";
+
 typedef enum {
   RETURN_TYPE_SUCCESS,
   RETURN_TYPE_ERROR,
@@ -15,19 +17,34 @@ typedef struct {
 } ErrBox;
 
 typedef struct {
-  char initializer_var;
-  char action;
+  char **args;
+  unsigned char arg_count;
+} Arguments;
+
+typedef struct {
   char *key;
   char *value;
-} Command;
+} KVBox;
+
+Arguments *Arguments_init(char **args, unsigned char arg_count) {
+  Arguments *self = malloc(sizeof(Arguments));
+  self->args = args;
+  self->arg_count = arg_count;
+
+  return self;
+}
+
+void ErrBox_free(ErrBox *self) {
+  free(self->ret_value);
+  free(self->err_desc);
+  free(self);
+}
 
 ErrBox *ErrBox_init_null() {
   ErrBox *ret = malloc(sizeof(ErrBox));
-
   ret->ret_outcome = RETURN_TYPE_ERROR;
   ret->ret_value = NULL;
   ret->err_desc = NULL;
-
   return ret;
 }
 
@@ -37,65 +54,47 @@ ErrBox *ErrBox_init(RETURN_TYPE outcome, void *value, char *description) {
   ret->ret_outcome = outcome;
   ret->ret_value = value;
   ret->err_desc = description;
-
   return ret;
 }
 
-Command *Command_init(char action, char *key, char *value) {
-  Command *ret = malloc(sizeof(Command));
-  ret->action = action;
-  ret->key = key;
-  ret->value = value;
-  ret->initializer_var = 3;
+ErrBox *KVBox_init_from_data(const char *data) {
+  KVBox *output = malloc(sizeof(KVBox));
+  if (output == NULL) {
+    return ErrBox_init(RETURN_TYPE_ERROR, NULL, "malloc failed\n");
+  }
+  output->key = NULL;
+  output->value = NULL;
 
-  return ret;
-}
+  size_t key_size = 0;
+  for (size_t data_iter = 0; data_iter < strlen(data); data_iter++) {
+    if (data[data_iter] == ',') {
+      output->key = malloc(data_iter * sizeof(char));
+      output->value = malloc(((strlen(data) - data_iter) + 1) * sizeof(char));
 
-Command *Command_init_null() {
-  Command *ret = malloc(sizeof(Command));
-  ret->action = 0;
-  ret->key = NULL;
-  ret->value = NULL;
-  ret->initializer_var = 0;
+      memset(output->key, '\0', data_iter);
+      memset(output->value, '\0', ((strlen(data) - data_iter) + 1));
 
-  return ret;
-}
-
-ErrBox *Command_part_init(Command *self, char *part) {
-  ErrBox *ret = NULL;
-
-  switch (self->initializer_var) {
-  case 0:
-    if (strlen(part) == 1) {
-      self->action = *part;
-    } else {
-      ret = ErrBox_init(
-          RETURN_TYPE_ERROR, NULL,
-          "error: action part should be exactly 1 character long\n");
+      strncpy(output->key, data, data_iter - 1);
+      strcpy(output->value, data + (data_iter + 1));
     }
-    ret = ErrBox_init(RETURN_TYPE_SUCCESS, NULL, NULL);
-    break;
-
-  case 1:
-    self->key = malloc((strlen(part) + 1) * sizeof(char));
-    strcpy(self->key, part);
-    ret = ErrBox_init(RETURN_TYPE_SUCCESS, NULL, NULL);
-    break;
-
-  case 2:
-    self->value = malloc((strlen(part) + 1) * sizeof(char));
-    strcpy(self->value, part);
-    printf("str: %s, len: %lu\n", self->value, strlen(part));
-    ret = ErrBox_init(RETURN_TYPE_SUCCESS, NULL, NULL);
-    break;
-
-  default:
-    return ErrBox_init(RETURN_TYPE_ERROR, NULL,
-                       "error: Command is already initialized\n");
   }
 
-  self->initializer_var++;
-  return ret;
+  if (output->key == NULL || output->value == NULL) {
+    return ErrBox_init(RETURN_TYPE_ERROR, NULL,
+                       "Failed initializing kv data\n");
+  }
+  return ErrBox_init(RETURN_TYPE_SUCCESS, output, NULL);
+}
+
+ErrBox *KVBox_init(char *key, char *value) {
+  KVBox *output = malloc(sizeof(KVBox));
+  if (output == NULL) {
+    return ErrBox_init(RETURN_TYPE_ERROR, NULL, "malloc failed\n");
+  }
+  output->key = key;
+  output->value = value;
+
+  return ErrBox_init(RETURN_TYPE_SUCCESS, output, NULL);
 }
 
 void print_usage() {
@@ -105,58 +104,207 @@ void print_usage() {
 // AKSHUALLY duo commas + \0
 ErrBox *tres_commas_split(const char *strbuf) {
   unsigned char comma_count = 0;
-  Command *cmd = Command_init_null();
   unsigned char point_after_last_comma = 0;
+  unsigned char curr_arg_count = 0;
+  unsigned char total_arg_count = 0;
 
+  switch (strbuf[0]) {
+  case 'p':
+    total_arg_count = 3;
+    break;
+  case 'g':
+    total_arg_count = 2;
+    break;
+  case 'd':
+    total_arg_count = 2;
+    break;
+  case 'c':
+    total_arg_count = 1;
+    break;
+  case 'a':
+    total_arg_count = 1;
+    break;
+  default:
+    return ErrBox_init(RETURN_TYPE_ERROR, NULL, "Invalid argument\n");
+  }
+
+  char **args = malloc(total_arg_count * sizeof(char *));
   for (int str_iter = 0; str_iter <= strlen(strbuf); str_iter++) {
-    if ((*(strbuf + str_iter) == ',' || *(strbuf + str_iter) == '\0') &&
-        comma_count < 3) {
-      // distance from first letter to end + nullbyte
-      char string_split_buffer[str_iter - point_after_last_comma + 1];
-      // zero out memory because of skill issues
-      memset(string_split_buffer, '\0', sizeof(string_split_buffer));
-      // src, argument+offset, distance
-      strncpy(string_split_buffer, strbuf + point_after_last_comma,
-              (str_iter - point_after_last_comma));
-
-      ErrBox *output = Command_part_init(cmd, string_split_buffer);
-      if (output->ret_outcome == RETURN_TYPE_ERROR) {
-        return output;
+    if ((*(strbuf + str_iter) == ',' || *(strbuf + str_iter) == '\0')) {
+      curr_arg_count++;
+      if (((str_iter - point_after_last_comma) > 1) && curr_arg_count == 1) {
+        free(args);
+        return ErrBox_init(RETURN_TYPE_ERROR, NULL,
+                           "First argument should be 1 letter\n");
       }
+      if (curr_arg_count > total_arg_count) {
+        for (int args_count = 0; args_count < total_arg_count; args_count++) {
+          free(args[args_count]);
+        }
+        free(args);
+        return ErrBox_init(RETURN_TYPE_ERROR, NULL,
+                           "Too many arguments provided\n");
+      }
+
+      // distance from first letter to end + nullbyte
+      args[curr_arg_count - 1] =
+          malloc((str_iter - point_after_last_comma + 1) * sizeof(char));
+      // zero out memory because of skill issues
+      memset(args[curr_arg_count - 1], '\0',
+             strlen(args[curr_arg_count - 1]) + 1);
+      // src, argument+offset, distance
+      strncpy(args[curr_arg_count - 1], strbuf + point_after_last_comma,
+              (str_iter - point_after_last_comma));
 
       comma_count++;
       point_after_last_comma = str_iter + 1;
     }
   }
-  if (comma_count != 3) {
-    ErrBox *ret =
-        ErrBox_init(RETURN_TYPE_ERROR, NULL,
-                    "error: invalid operation format (comma_count != 2)\n");
-    return ret;
-  }
-
-  return ErrBox_init(RETURN_TYPE_SUCCESS, cmd, NULL);
+  Arguments *output = Arguments_init(args, total_arg_count);
+  return ErrBox_init(RETURN_TYPE_SUCCESS, output, NULL);
 }
 
+ErrBox *find_key(const char *key) {
+  FILE *database = fopen(KV_DATABASE_PATH, "r");
+  if (database == NULL) {
+    return ErrBox_init(RETURN_TYPE_ERROR, NULL, "fopen failed\n");
+  }
+
+  size_t line_size = 0;
+  char *line = NULL;
+  while (getline(&line, &line_size, database)) {
+    if (line[0] == '\0') {
+      break;
+    }
+
+    printf("line: %s\n", line);
+    ErrBox *data = KVBox_init_from_data(line);
+    if (data->ret_outcome == RETURN_TYPE_SUCCESS) {
+      KVBox *kv_data = data->ret_value;
+      if (!strcmp(kv_data->key, key)) {
+        fclose(database);
+        free(line);
+        return data;
+      } else {
+        free(kv_data->value);
+        free(kv_data->key);
+        free(kv_data);
+      }
+    } else {
+      fclose(database);
+      return data;
+    }
+  }
+
+  fclose(database);
+  return ErrBox_init(RETURN_TYPE_SUCCESS, NULL, NULL);
+}
+
+ErrBox *action_put(const char *key, const char *value) {
+  FILE *database = fopen(KV_DATABASE_PATH, "a+");
+  ErrBox *output = NULL;
+  if (database == NULL) {
+    return ErrBox_init(RETURN_TYPE_ERROR, NULL, "fopen failed\n");
+  }
+
+  ErrBox *data = find_key(key);
+  if (data->ret_outcome == RETURN_TYPE_SUCCESS) {
+    if (data->ret_value != NULL) {
+      free(data);
+      fclose(database);
+      return ErrBox_init(RETURN_TYPE_ERROR, NULL, "They key already exists\n");
+    } else {
+      // Put into file
+      fprintf(database, "%s,%s\n", key, value);
+    }
+  } else {
+    fclose(database);
+    return data;
+  }
+
+  fclose(database);
+  return ErrBox_init(RETURN_TYPE_SUCCESS, NULL, NULL);
+}
+
+/* ErrBox *action_get(const char *key) { */
+/*   lol; */
+/*   return; */
+/* } */
+/**/
+/* ErrBox *action_delete(const char *key) { */
+/*   lol; */
+/*   return; */
+/* } */
+/**/
+/* ErrBox *action_clear() { */
+/*   lol; */
+/*   return; */
+/* } */
+/**/
+/* ErrBox *action_all() { */
+/*   lol; */
+/*   return; */
+/* } */
+/**/
 int main(int argc, char *argv[]) {
   if (argc == 1) {
     print_usage();
   }
 
   for (size_t arg_iter = 1; arg_iter < argc; arg_iter++) {
-    ErrBox *lol = tres_commas_split(argv[arg_iter]);
-    if (lol->ret_outcome == RETURN_TYPE_SUCCESS) {
-      Command *lolol = (Command *)lol->ret_value;
-      printf("%c, %s, %s\n", lolol->action, lolol->key, lolol->value);
-      free(lolol->value);
-      free(lolol->key);
-    } else {
-      printf("%s\n", lol->err_desc);
+    ErrBox *split_output = tres_commas_split(argv[arg_iter]);
+    if (split_output == NULL) {
+      fprintf(stdout, "Null ptr received: 1\n");
+      return 1;
     }
 
-    free(lol->err_desc);
-    free(lol->ret_value);
-    free(lol);
+    Arguments *arguments_struct = NULL;
+    if (split_output->ret_outcome == RETURN_TYPE_SUCCESS) {
+      arguments_struct = (Arguments *)split_output->ret_value;
+      if (arguments_struct == NULL) {
+        fprintf(stdout, "Null ptr received: 2\n");
+        return 1;
+      }
+
+      ErrBox *output = NULL;
+      switch (arguments_struct->args[0][0]) {
+      case 'p':
+        output =
+            action_put(arguments_struct->args[1], arguments_struct->args[2]);
+        break;
+      case 'g':
+        /* action_get(arguments_struct->args[1]); */
+        break;
+      case 'd':
+        /* action_delete(arguments_struct->args[1]); */
+        break;
+      case 'c':
+        /* action_clear(); */
+        break;
+      case 'a':
+        /* action_all(); */
+        break;
+      default:
+        fprintf(stdout, "Invalid argument\n");
+        return 1;
+      }
+
+      if (output->ret_outcome != RETURN_TYPE_SUCCESS) {
+        printf("%s\n", output->err_desc);
+      }
+      free(output);
+    } else {
+      printf("%s\n", split_output->err_desc);
+    }
+
+    for (int args_iter = 0; args_iter < arguments_struct->arg_count;
+         args_iter++) {
+      free(arguments_struct->args[args_iter]);
+    }
+    free(arguments_struct->args);
+    free(split_output->err_desc);
+    free(split_output->ret_value);
+    free(split_output);
   }
   return 0;
 }
